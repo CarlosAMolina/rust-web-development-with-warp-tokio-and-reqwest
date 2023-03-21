fc56c84b08f55f355f653eb6623be4a11353633e
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-03-21 22:27:19 +0100

    Partial commit add question

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,29 @@
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let question = Question::new(
+        QuestionId("1".to_string()),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        ["faq".to_string()],
+    );
+    println!("{}", question);
 }
