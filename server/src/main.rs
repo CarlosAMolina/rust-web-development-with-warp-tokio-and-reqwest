0a4d91da06017859cbd7488dc8144859ea87da48
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-03-22 23:27:20 +0100

    Add question struct

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,7 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+#[derive(Debug)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -5,7 +9,21 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
+#[derive(Debug)]
 struct QuestionId(String);
+
+impl FromStr for QuestionId {
+   type Err = std::io::Error;
+
+   fn from_str(id: &str) -> Result<Self, Self::Err> {
+       match id.is_empty() {
+           false => Ok(QuestionId(id.to_string())),
+           true => Err(
+             Error::new(ErrorKind::InvalidInput, "No id provided")
+           ),
+       }
+   }
+}
 
 impl Question {
     fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
@@ -20,10 +38,10 @@ impl Question {
 
 fn main() {
     let question = Question::new(
-        QuestionId("1".to_string()),
+        QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
         "Content of question".to_string(),
-        ["faq".to_string()],
+        Some(vec!("faq".to_string())),
     );
-    println!("{}", question);
+    println!("{:?}", question);
 }
