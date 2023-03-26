137990bfd20185ec2640eceebd50dbcd93b07e47
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-03-26 14:40:35 +0200

    Add run web server

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This is the project realized in the book `Rust Web Development: With warp, tokio, and reqwest`.
 
+## Run
+
+```bash
+cargo run
+curl 127.0.0.1:3030
+```
+
 ## Resources
 
 Book code:
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
+use warp::Filter;
+
 #[derive(Debug)]
 struct Question {
     id: QuestionId,
@@ -13,16 +15,14 @@ struct Question {
 struct QuestionId(String);
 
 impl FromStr for QuestionId {
-   type Err = std::io::Error;
+    type Err = std::io::Error;
 
-   fn from_str(id: &str) -> Result<Self, Self::Err> {
-       match id.is_empty() {
-           false => Ok(QuestionId(id.to_string())),
-           true => Err(
-             Error::new(ErrorKind::InvalidInput, "No id provided")
-           ),
-       }
-   }
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
 }
 
 impl Question {
@@ -36,12 +36,17 @@ impl Question {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
         "Content of question".to_string(),
-        Some(vec!("faq".to_string())),
+        Some(vec!["faq".to_string()]),
     );
     println!("{:?}", question);
+
+    let hello = warp::get().map(|| format!("Hello, World!"));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
