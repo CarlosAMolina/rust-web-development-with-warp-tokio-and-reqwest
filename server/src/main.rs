fb69486d55b505edaf4ffd36a95fba1b68fbac58
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-04-01 03:04:32 +0200

    Add section 3.3 Handling CORS headers

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,10 @@ use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
 use serde::Serialize;
-use warp::{Filter, reject::Reject, Rejection, Reply, http::StatusCode};
+use warp::{
+    filters::cors::CorsForbidden, http::Method, http::StatusCode, reject::Reject, Filter,
+    Rejection, Reply,
+};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -47,46 +50,56 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
         QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
         "Content of question".to_string(),
-        Some(vec!("faq".to_string())),
-    ); 
+        Some(vec!["faq".to_string()]),
+    );
 
-     match question.id.0.parse::<i32>() {
-     Err(_) =>  {
-     Err(warp::reject::custom(InvalidId))
-     },
-     Ok(_) => {
-     Ok(warp::reply::json(
-     &question
-     ))
-     }
-     }
- 
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+        Ok(_) => Ok(warp::reply::json(&question)),
+    }
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(_invalid_id) = r.find::<InvalidId>() {
+    println!("{:?}", r);
+    // I think the CorsForbidden is never matched in this function.
+    // For example, with an invalid header, the previous println is not executed:
+    // ```bash
+    // curl -X OPTIONS localhost:3030/questions \
+    //      -H "Access-Control-Request-Method: PUT" \
+    //      -H "Access-Control-Request-Headers: invalid-header" \
+    //      -H "Origin: https://not-origin.io" -verbose
+    // ```
+    if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
-            "No valid ID presented",
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(_invalid_id) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
         Ok(warp::reply::with_status(
-            "Route not found",
+            "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
 }
 
-
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and_then(get_questions)
         .recover(return_error);
- 
-    let routes = get_items;
+
+    let routes = get_items.with(cors);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
