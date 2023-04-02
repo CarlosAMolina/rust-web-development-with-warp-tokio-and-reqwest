eb983396539e40a090b05f5496bbbbf4d2a371e3
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-04-03 00:54:36 +0200

    Apply cargo fmt

--- server/src/main.rs
+++ server/src/main.rs
@@ -22,7 +22,6 @@ struct Question {
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 struct QuestionId(String);
 
-
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 struct AnswerId(String);
 
@@ -32,7 +31,6 @@ struct Answer {
     content: String,
     question_id: QuestionId,
 }
-
 
 fn extract_pagination(
     params: HashMap<String, String>,
@@ -80,11 +78,15 @@ async fn get_answers_of_question(
     store: Store,
 ) -> Result<impl warp::Reply, warp::Rejection> {
     match store.questions.read().await.get(&QuestionId(id)) {
-        Some(question) =>  {
+        Some(question) => {
             let answers_all: Vec<Answer> = store.answers.read().await.values().cloned().collect();
-            let answers: Vec<Answer> = answers_all.iter().filter(|answer| answer.question_id == question.id).cloned().collect();
+            let answers: Vec<Answer> = answers_all
+                .iter()
+                .filter(|answer| answer.question_id == question.id)
+                .cloned()
+                .collect();
             Ok(warp::reply::json(&answers))
-        },
+        }
         None => return Err(warp::reject::custom(Error::QuestionNotFound)),
     }
 }
@@ -105,12 +107,9 @@ async fn get_questions(
     }
 }
 
-async fn get_question(
-    id: String,
-    store: Store,
-) -> Result<impl warp::Reply, warp::Rejection> {
+async fn get_question(id: String, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
     match store.questions.read().await.get(&QuestionId(id)) {
-        Some(question) =>  Ok(warp::reply::json(&question)),
+        Some(question) => Ok(warp::reply::json(&question)),
         None => return Err(warp::reject::custom(Error::QuestionNotFound)),
     }
 }
@@ -159,8 +158,8 @@ async fn add_answer(
                     StatusCode::RANGE_NOT_SATISFIABLE,
                 ));
             }
-        },
-        None => return Err(warp::reject::custom(Error::MissingParameters))
+        }
+        None => return Err(warp::reject::custom(Error::MissingParameters)),
     }
     match params.get("questionId") {
         Some(question_id) => {
@@ -170,18 +169,20 @@ async fn add_answer(
                     StatusCode::RANGE_NOT_SATISFIABLE,
                 ));
             }
-        },
-        None => return Err(warp::reject::custom(Error::MissingParameters))
+        }
+        None => return Err(warp::reject::custom(Error::MissingParameters)),
     }
     let answer_id = {
         let answer_ids: Vec<AnswerId> = store.answers.read().await.keys().cloned().collect();
         if answer_ids.is_empty() {
             0
-        }
-        else {
-            let mut answer_ids_usize: Vec<usize> = answer_ids.iter().map(|answer_id| answer_id.0.parse::<usize>().unwrap()).collect();
+        } else {
+            let mut answer_ids_usize: Vec<usize> = answer_ids
+                .iter()
+                .map(|answer_id| answer_id.0.parse::<usize>().unwrap())
+                .collect();
             answer_ids_usize.sort_unstable();
-            let max_answer_id = answer_ids_usize[answer_ids_usize.len()-1];
+            let max_answer_id = answer_ids_usize[answer_ids_usize.len() - 1];
             max_answer_id + 1
         }
     };
@@ -195,7 +196,7 @@ async fn add_answer(
         question_ids.contains(&question_id)
     };
     if !exists_question_id {
-        return Err(warp::reject::custom(Error::QuestionNotFound))
+        return Err(warp::reject::custom(Error::QuestionNotFound));
     }
     let answer = Answer {
         id: AnswerId(answer_id.to_string()),
@@ -207,7 +208,7 @@ async fn add_answer(
         .write()
         .await
         .insert(answer.id.clone(), answer);
-    return Ok(warp::reply::with_status("Answer added", StatusCode::OK))
+    return Ok(warp::reply::with_status("Answer added", StatusCode::OK));
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
@@ -276,7 +277,7 @@ impl std::fmt::Display for Error {
             Error::MissingParameters => write!(f, "Missing parameter"),
             Error::ParseError(ref err) => {
                 write!(f, "Cannot parse parameter: {}", err)
-            },
+            }
             Error::StartGreaterThanEnd => write!(f, "The start is greater than the end"),
         }
     }
