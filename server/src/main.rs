c6c186e8adef250fef96e077776adb34397f01a2
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-04-02 06:10:05 +0200

    Add 4.2.1 Updating our data in a thread-safe way

--- server/src/main.rs
+++ server/src/main.rs
@@ -62,6 +62,19 @@ async fn get_questions(
     }
 }
 
+async fn add_question(
+    store: Store,
+    question: Question,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    store
+        .questions
+        .write()
+        .await
+        .insert(question.id.clone(), question);
+
+    Ok(warp::reply::with_status("Question added", StatusCode::OK))
+}
+
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     println!("{:?}", r);
     // Example request to call this function:
@@ -148,10 +161,20 @@ async fn main() {
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and(warp::query())
-        .and(store_filter)
+        .and(store_filter.clone())
         .and_then(get_questions);
 
-    let routes = get_questions.with(cors).recover(return_error);
+    let add_question = warp::post()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and(warp::body::json())
+        .and_then(add_question);
+
+    let routes = get_questions
+        .or(add_question)
+        .with(cors)
+        .recover(return_error);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
