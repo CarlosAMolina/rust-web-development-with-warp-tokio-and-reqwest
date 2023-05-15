ab7f91bb6106b932b28bd83de90d45bc314035c6
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-05-15 18:29:20 +0200

    Add log incoming HTTP request

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,6 +16,10 @@ async fn main() {
     log::info!("This is info!");
     log::warn!("This is a warning!");
 
+    let log = warp::log::custom(|info| {
+        eprintln!("{} {} {}", info.method(), info.path(), info.status(),);
+    });
+
     let store = store::Store::new();
     let store_filter = warp::any().map(move || store.clone());
 
@@ -91,6 +95,7 @@ async fn main() {
         .or(update_question)
         .or(delete_question)
         .with(cors)
+        .with(log)
         .recover(return_error);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
