0aaced23e489f2961e6b7c3576adf50414fcf977
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-06-01 22:27:27 +0200

    Partial commit Improve error handling

--- server/src/routes/question.rs
+++ server/src/routes/question.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::env;
 
@@ -7,6 +8,24 @@ use warp::http::StatusCode;
 use crate::store::Store;
 use crate::types::pagination::{extract_pagination, Pagination};
 use crate::types::question::{NewQuestion, Question, QuestionId};
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct BadWord {
+   original: String,
+   word: String,
+   deviations: i64,
+   info: i64,
+   #[serde(rename = "replacedLen")]
+   replaced_len: i64,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct BadWordsResponse {
+   content: String,
+   bad_words_total: i64,
+   bad_words_list: Vec<BadWord>,
+   censored_content: String,
+}
 
 pub async fn add_question(
     store: Store,
@@ -18,30 +37,44 @@ pub async fn add_question(
     let res = client
         .post("https://api.apilayer.com/bad_words?censor_character=*")
         .header("apikey", api_key)
-        .body("a list with shit words")
+        .body(new_question.content.clone()) // TODO try removing clone()
         .send()
         .await
         .map_err(|e| handle_errors::Error::ExternalAPIError(e))?;
-    // The raised exceptions do not cover all cases:
-    // https://docs.rs/reqwest/latest/reqwest/struct.RequestBuilder.html#method.send
-    // Add raise exception with nonsucess status code in the response.
-    // Example, if we set the apiKey header wrong.
-    match res.error_for_status() {
-        Ok(res) => {
-            let res = res
-                .text()
-                .await
-                .map_err(|e| handle_errors::Error::ExternalAPIError(e))?;
-            println!("{}", res); // TODO rm
-            match store.add_question(new_question).await {
-                Ok(_) => Ok(warp::reply::with_status("Question added", StatusCode::OK)),
-                Err(e) => Err(warp::reject::custom(e)),
-            }
+    if !res.status().is_success() {
+        if res.status().is_client_error() {
+            let err = transform_error(res).await;
+            return Err(handle_errors::Error::ServerError(err));
         }
-        Err(err) => Err(warp::reject::custom(
-            handle_errors::Error::ExternalAPIError(err),
-        )),
     }
+
+    // TODO RM // The raised exceptions do not cover all cases:
+    // TODO RM // https://docs.rs/reqwest/latest/reqwest/struct.RequestBuilder.html#method.send
+    // TODO RM // Add raise exception with nonsucess status code in the response.
+    // TODO RM // Example, if we set the apiKey header wrong.
+    // TODO RM match res.error_for_status() {
+    // TODO RM     Ok(res) => {
+    // TODO RM         let res = res
+    // TODO RM             .text()
+    // TODO RM             .await
+    // TODO RM             .map_err(|e| handle_errors::Error::ExternalAPIError(e))?;
+    // TODO RM         println!("{}", res); // TODO rm
+    // TODO RM         match store.add_question(new_question).await {
+    // TODO RM             Ok(_) => Ok(warp::reply::with_status("Question added", StatusCode::OK)),
+    // TODO RM             Err(e) => Err(warp::reject::custom(e)),
+    // TODO RM         }
+    // TODO RM     }
+    // TODO RM     Err(err) => Err(warp::reject::custom(
+    // TODO RM         handle_errors::Error::ExternalAPIError(err),
+    // TODO RM     )),
+    // TODO RM }
+
+    // TODO USE let res = res.json::  
+    // TODO USE ()     .await     .map_err(|e| handle_errors::Error::ExternalAPIError(e))?;    let content = res.censored_content;    let question = NewQuestion {     title: new_question.title,     content,     tags: new_question.tags,   };    match store.add_question(question).await {     Ok(question) => Ok(warp::reply::json(&question)), ❺     Err(e) => Err(warp::reject::custom(e)),   } } async fn transform_error(   res: reqwest::Response ) -> handle_errors::APILayerError { ❻   handle_errors::APILayerError {     status: res.status().as_u16(),     message: res.json::
+    // TODO USE ().await.unwrap().message,   } }
+
+
+
 }
 
 // TODO check what happen y ID not in db
